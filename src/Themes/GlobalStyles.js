bd7feca26de71b2910cc27d571ff9264ba6e62ff
Patryk Gajek <pgajek94@gmail.com>
2019-07-15 20:06:10 +0200

    Global Styles

--- src/Themes/GlobalStyles.js
+++ src/Themes/GlobalStyles.js
@@ -1,13 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Inconsolata&display=swap');
 *, *::after, *::before{
     box-sizing: border-box;
     padding: 0;
     margin: 0;
 }
+html{
+    font-size: 62.5%
+}
 body{
     background-color: #222222;
+    font-size: 1.6em;
+    font-family: 'Inconsolata', monospace;
 }
 `;
 
