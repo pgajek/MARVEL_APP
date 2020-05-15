c9cacc38913e4316753e0534579a13eb00d105b7
Patryk Gajek <pgajek94@gmail.com>
2020-05-15 13:35:31 +0200

    Updates

---
+++ precache-manifest.8d8ed6d21855923d5769b58ef9ff0df1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "588261e4919bbae35d1d",
+    "url": "/MARVEL_APP/static/js/main.9eba2f7d.chunk.js"
+  },
+  {
+    "revision": "f128fd4a8ac11f0ca31c",
+    "url": "/MARVEL_APP/static/js/runtime~main.576f9613.js"
+  },
+  {
+    "revision": "59740d9cb65336fb0a74",
+    "url": "/MARVEL_APP/static/js/2.5a5bc9dd.chunk.js"
+  },
+  {
+    "revision": "3a36d79cdda4606c11565122b4edd691",
+    "url": "/MARVEL_APP/static/media/thanos_snap.3a36d79c.png"
+  },
+  {
+    "revision": "b746c7d7681eb0932c2c5cd0cf7bc762",
+    "url": "/MARVEL_APP/static/media/thanos_snap_sound.b746c7d7.mp3"
+  },
+  {
+    "revision": "f2ab78d31c81c39ca073d19d4b07de2b",
+    "url": "/MARVEL_APP/index.html"
+  }
+];
